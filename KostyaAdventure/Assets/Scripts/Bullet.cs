9fa938d4da6f13017a9226e0e11f6add7b5bc48c
cross_ <97113984+cross12345@users.noreply.github.com>
2022-01-12 15:20:07 +0600

    Основные фичи
    Добавил тайлсет(скачал)
    настроил атаку
    сделана пикселизация(особо видно на поворачиваемых объектах)

--- KostyaAdventure/Assets/Scripts/Bullet.cs
+++ KostyaAdventure/Assets/Scripts/Bullet.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] ParticleSystem destroyParticle;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBullet();
+    }
+    public void DestroyBullet()
+    {
+        Instantiate(destroyParticle, transform.position, Quaternion.Euler(0, 0, 0));
         Destroy(gameObject);
     }
 }
